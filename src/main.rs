788f9a4b6a9f31ce8c884169ccfa0b7d2d8ec1b0
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-25 21:40:52 -0700

    define error hook fn

--- src/main.rs
+++ src/main.rs
@@ -32,3 +32,19 @@ fn restore_terminal() -> color_eyre::Result<()> {
     Ok(())
 }
 
+fn init_error_hooks() -> color_eyre::Result<()> {
+    let (panic, error) = HookBuilder::default().into_hooks();
+    let panic = panic.into_panic_hook();
+    let error = error.into_eyre_hook();
+
+    color_eyre::eyre::set_hook(Box::new(move |e| {
+        let _ = restore_terminal();
+        error(e)
+    }))?;
+
+    std::panic::set_hook(Box::new(move |info| {
+        let _ = restore_terminal();
+        panic(info);
+    }));
+    Ok(())
+}
