ad76661107bc991a38ccc9df84f55c7f335ea356
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-25 22:44:12 -0700

    render example paragraph

--- src/ui.rs
+++ src/ui.rs
@@ -7,3 +7,24 @@ use crate::app::AppState;
 
 impl Widget for &mut AppState {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        let layout = Layout::vertical([Constraint::Min(3)]);
+        let [main] = layout.areas(area);
+
+        // Delete This Paragraph!
+        Paragraph::new(
+            "DELETE @ -> src\\app.rs -> fn render() {\n// Delete This Paragraph!    
+            .block(Block::bordered().title(\"Delete Me!\"))
+            .fg(Color::Red)
+            .render(main, buf)
+        };
+            ",
+        )
+        .block(
+            Block::bordered()
+                .title("Delete Me!")
+                .padding(Padding::new(1, 1, 1, 1)),
+        )
+        .fg(Color::Red)
+        .render(main, buf);
+    }
+}
