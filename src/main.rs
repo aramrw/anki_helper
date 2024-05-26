5160347ede84fc9cc48aa553f06ec06312cae3eb
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-25 22:44:48 -0700

    lint

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![allow(clippy::enum_glob_use, clippy::wildcard_imports)]
 
-use std::{error::Error, io, io::stdout};
 mod app;
 mod ui;
 mod keybinds;
@@ -8,7 +7,6 @@ mod keybinds;
 use std::{error::Error, io::stdout};
 use color_eyre::config::HookBuilder;
 use crossterm::{
-    event::{self, Event, KeyCode, KeyEventKind},
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
