6e6b7a337526c92161571adb200f5c2433d9fee0
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-25 22:43:04 -0700

    define AppState run fn

--- src/app.rs
+++ src/app.rs
@@ -10,6 +10,21 @@ impl AppState {
     }
 }
 
+impl AppState {
+    pub fn run(&mut self, mut term: Terminal<impl Backend>) -> io::Result<()> {
+        loop {
+            self.draw(&mut term)?;
+
+            // handle key events & keybindings
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('Q') {
+                    return Ok(());
+                }
+                // src/keybinds.rs
+                self.handle_keybinds(key)?
+            }
+        }
+    }
 
     fn draw(&mut self, term: &mut Terminal<impl Backend>) -> io::Result<()> {
         term.draw(|f| f.render_widget(self, f.size()))?;
