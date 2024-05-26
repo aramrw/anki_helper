78806c70ddf85e3de7f591d36a0003235250a298
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-25 22:42:33 -0700

    setup AppState struct

--- src/app.rs
+++ src/app.rs
@@ -2,3 +2,11 @@ use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::prelude::*;
 use std::io;
 
+pub struct AppState {}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self { /* add variables to AppState struct */ }
+    }
+}
+
