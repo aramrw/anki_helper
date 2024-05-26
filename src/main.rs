40d2c70019cbbf5bc129df5317149f3e6f4cca4c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-25 21:39:59 -0700

    import main libs

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![allow(clippy::enum_glob_use, clippy::wildcard_imports)]
+
+use std::{error::Error, io, io::stdout};
+
+use color_eyre::config::HookBuilder;
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::prelude::*;
+
