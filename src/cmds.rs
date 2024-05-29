ab05336823d24c80d9e43c83313c09cbf118e666
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-28 18:11:17 -0700

    define read_words_file for AppState

--- src/cmds.rs
+++ src/cmds.rs
@@ -3,3 +3,20 @@ use std::io::{self, prelude::*, BufReader};
 
 use crate::app::*;
 
+impl AppState {
+    pub fn read_words_file(&mut self) -> io::Result<()> {
+        let file = File::open("words.txt")?;
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            let line = line?;
+
+            for word in line.split_whitespace() {
+                self.expressions
+                    .push(Expression::from(word.to_string(), None));
+            }
+        }
+
+        Ok(())
+    }
+
