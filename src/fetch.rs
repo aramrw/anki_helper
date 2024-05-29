2cc3e82c5949e6173a5630adb20a7a1b7dad1e6c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-29 12:51:35 -0700

    define main keys & values

--- src/fetch.rs
+++ src/fetch.rs
@@ -7,6 +7,15 @@ use std::thread;
 use std::time;
 
 #[derive(Serialize, Deserialize, Debug)]
+struct Main {
+    category_count: CategoryCount,
+    deck_count: DeckCount,
+    dictionary: Vec<Vec<Glossary>>,
+    exact_match: Option<String>,
+    examples: Vec<Example>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct JsonSchema {
     data: Vec<Main>,
 }
