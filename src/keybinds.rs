9459563e1a5fea7e9df2d93063cbfc4302d2ce52
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-29 12:58:51 -0700

    breakdown handle keybinds

--- src/keybinds.rs
+++ src/keybinds.rs
@@ -1,23 +1,24 @@
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::event::{/*self, Event */ KeyCode, KeyEvent, KeyEventKind};
 //use ratatui::prelude::*;
-use std::io;
-
 use crate::app::{AppState, SelectMode};
+use std::{io, thread, time::*};
 
 impl AppState {
-    pub fn handle_keybinds(&mut self, key: KeyEvent) -> io::Result<()> {
+    pub async fn handle_keybinds(&mut self, key: KeyEvent) -> io::Result<()> {
         match self.select_mode {
             SelectMode::Expressions if key.kind == KeyEventKind::Press => match key.code {
+                KeyCode::Enter => self.fetch_sentences().await,
                 KeyCode::Down => self.select_next_exp(),
                 KeyCode::Up => self.select_prev_exp(),
-                _ => {},
-            }
+                _ => {}
+            },
             SelectMode::Sentences if key.kind == KeyEventKind::Press => match key.code {
                 //KeyCode::Down => self.select_next(),
-                _ => {},
-            }
-            _ => {},
-        } 
+                _ => {}
+            },
+            _ => {}
+        }
 
         Ok(())
     }
