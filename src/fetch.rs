a5710c0d5fbbdc83b4665d4398662815e20472c9
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-29 12:56:43 -0700

    define example keys & values

--- src/fetch.rs
+++ src/fetch.rs
@@ -7,6 +7,33 @@ use std::thread;
 use std::time;
 
 #[derive(Serialize, Deserialize, Debug)]
+struct Example {
+    author_japanese: Option<String>,
+    category: String,
+    channel: Option<String>,
+    deck_id: u16,
+    deck_name: String,
+    deck_name_japanese: Option<String>,
+    episode: StringOru64,
+    id: u64,
+    image_url: Option<String>,
+    position: u64,
+    sentence: String,
+    sentence_id: String,
+    sentence_with_furigana: String,
+    sound_begin: StringOru64,
+    sound_end: StringOru64,
+    sound_url: Option<String>,
+    tags: Vec<String>,
+    timestamp: Option<String>,
+    translation: String,
+    translation_word_index: Vec<usize>,
+    translation_word_list: VecStringOrString,
+    word_index: Vec<usize>,
+    word_list: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Main {
     category_count: CategoryCount,
     deck_count: DeckCount,
