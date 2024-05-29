89cbc13a54c9906a93dccff7aec45bc181b6a05d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-28 18:17:35 -0700

    lint

--- src/ui.rs
+++ src/ui.rs
@@ -1,23 +1,12 @@
 use ratatui::{
     prelude::*,
-    widgets::{Block, Padding, Paragraph},
     widgets::{Block, List, ListItem, Padding, Paragraph},
 };
 
-use crate::app::AppState;
 use crate::app::{AppState, SelectMode};
 
 impl Widget for &mut AppState {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let layout = Layout::vertical([Constraint::Min(3)]);
-        let [main] = layout.areas(area);
-
-        // Delete This Paragraph!
-        Paragraph::new(
-            "DELETE @ -> src\\app.rs -> fn render() {\n// Delete This Paragraph!    
-            .block(Block::bordered().title(\"Delete Me!\"))
-            .fg(Color::Red)
-            .render(main, buf)
         let layout = Layout::vertical([
             Constraint::Length(3),
             Constraint::Min(10),
