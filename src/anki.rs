c9575d803b36831d1c6e13296997d1d42b1aa420
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-29 21:55:18 -0700

    create note struct

--- src/anki.rs
+++ src/anki.rs
@@ -5,3 +5,11 @@ use reqwest;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+#[derive(Serialize, Deserialize)]
+struct Note {
+    id: u64,
+    fields: HashMap<String, String>,
+    audio: Vec<Media>,
+    picture: Option<Vec<Media>>,
+}
+
