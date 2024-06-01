dfb3dffff79ec0855cecf5550d5685fc20cdae4a
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-31 19:05:01 -0700

    define move left

--- src/input.rs
+++ src/input.rs
@@ -4,3 +4,8 @@ use ratatui::{
     widgets::{Block, Paragraph},
 };
 
+    pub fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.input.char_index.saturating_sub(1);
+        self.input.char_index = self.clamp_cursor(cursor_moved_left);
+    }
+
