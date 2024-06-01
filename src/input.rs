d86643f8085281f432790939a1e37c4de271a933
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-31 19:05:29 -0700

    define delete char

--- src/input.rs
+++ src/input.rs
@@ -29,3 +29,25 @@ use ratatui::{
             .unwrap_or(self.input.text.len())
     }
 
+    pub fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.input.char_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.input.char_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.input.text.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.input.text.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.input.text = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
