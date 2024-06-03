cf1229ccff06ccb8673386f0c7e6d91444d8c002
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-03 14:56:18 -0700

    update payload to include both downloading and using local audio

--- src/anki.rs
+++ src/anki.rs
@@ -11,7 +11,6 @@ use std::time::Instant;
 struct Note {
     id: u64,
     fields: HashMap<String, String>,
-    audio: Vec<Media>,
     audio: Option<Vec<Media>>,
     picture: Option<Vec<Media>>,
 }
@@ -46,8 +45,6 @@ struct Request<P: AnkiParams> {
 
 // other
 #[derive(Serialize, Deserialize)]
-struct ConfigJson {
-    fields: UserNoteFields,
 pub struct ConfigJson {
     pub fields: UserNoteFields,
     pub media_path: String,
@@ -89,8 +86,8 @@ impl AppState {
                 }
             };
 
-            let fields: UserNoteFields = match read_config() {
-                Ok(fields) => fields,
+            let config: ConfigJson = match read_config() {
+                Ok(config) => config,
                 Err(err) => {
                     self.err_msg = Some(format!("Error Reading Config: {}", err));
                     return;
@@ -106,8 +103,19 @@ impl AppState {
             };
 
             let filename = url_into_file_name(&sentence.audio_url);
-            let req: Request<UpdateNoteParams> =
-                into_update_note_req(note_id as u64, fields, sentence, filename);
+            let local_audio_url = if let Some(audio_data) = &sentence.audio_data {
+                Some(write_audio_bytes_file(&config.media_path, &filename, audio_data).unwrap())
+            } else {
+                None
+            };
+
+            let req: Request<UpdateNoteParams> = into_update_note_req(
+                note_id as u64,
+                &config.fields,
+                sentence,
+                filename,
+                local_audio_url,
+            );
             match post_note_update(req).await {
                 Ok(_) => {
                     let elapsed = instant.elapsed().as_secs();
@@ -208,18 +216,6 @@ fn format_sentence_field(field_name: &str, ik_sentence: &str) -> HashMap<String,
     map
 }
 
-#[allow(dead_code)]
-fn write_audio_bytes_file(filename: &str, bytes: &Vec<u8>) -> std::io::Result<String> {
-    //let dir = tempfile::tempdir()?;
-    let dir = std::path::Path::new("C:\\Users\\arami\\Desktop");
-    let file_path = dir.join(filename);
-    std::fs::write(&file_path, bytes)?;
-    //let audio_url = format!("file:\\{}", &file_path.to_string_lossy());
-    let audio_url = format!(
-        "[sound:{}]'.format({})",
-        file_path.to_string_lossy(),
-        filename
-    );
 fn format_local_audio_field(field_name: &str, url: &str) -> HashMap<String, String> {
     let mut map = HashMap::new();
     map.insert(field_name.to_string(), url.to_string());
@@ -240,11 +236,13 @@ fn write_audio_bytes_file(
 
 fn into_update_note_req(
     id: u64,
-    anki_fields: UserNoteFields,
+    anki_fields: &UserNoteFields,
     sentence: Sentence,
     filename: String,
+    local_audio_url: Option<String>,
 ) -> Request<UpdateNoteParams> {
     let sentence_field = format_sentence_field(&anki_fields.sentence, &sentence.sentence);
+
     let picture: Option<Vec<Media>> = match &sentence.img_url {
         Some(img_url) => vec![Media {
             url: img_url.clone(),
@@ -256,27 +254,36 @@ fn into_update_note_req(
         None => None,
     };
 
-    let audio: Vec<Media> = vec![Media {
-        url: sentence.audio_url,
-        filename,
-        skipHash: None,
-        fields: vec![anki_fields.sentence_audio.clone()],
-    }];
+    let local_audio_field: Option<HashMap<String, String>> =
+        local_audio_url.map(|local_audio_url| {
+            format_local_audio_field(&anki_fields.sentence_audio, &local_audio_url)
+        });
 
-    let note: Note = match picture {
+    let mut note: Note = match picture {
         Some(picture) => Note {
             id,
             fields: { sentence_field },
-            audio,
+            audio: None,
             picture: Some(picture),
         },
         None => Note {
             id,
             fields: { sentence_field },
-            audio,
+            audio: None,
             picture,
         },
     };
+
+    if let Some(audio_field) = local_audio_field {
+        note.fields.extend(audio_field);
+    } else {
+        note.audio = Some(vec![Media {
+            url: sentence.audio_url,
+            filename,
+            skipHash: None,
+            fields: vec![anki_fields.sentence_audio.clone()],
+        }]);
+    }
 
     let params = UpdateNoteParams { note };
 
@@ -287,10 +294,10 @@ fn into_update_note_req(
     }
 }
 
-pub fn read_config() -> Result<UserNoteFields, std::io::Error> {
+pub fn read_config() -> Result<ConfigJson, std::io::Error> {
     let config_path = "./config.json";
     let data = std::fs::read(config_path)?;
     let config: ConfigJson = serde_json::from_slice(&data)?;
 
-    Ok(config.fields)
+    Ok(config)
 }
