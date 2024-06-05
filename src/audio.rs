06dcc12aae376f56cbc4ed91eb56c26888b1ca4f
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-05 13:37:50 -0700

    impl rend splice page fn

--- src/audio.rs
+++ src/audio.rs
@@ -4,3 +4,13 @@ use ratatui::{
     widgets::{Block, List, ListItem, ListState, Paragraph},
 };
 
+    pub fn rend_splice_page(&self, area: Rect, buf: &mut Buffer) {
+        let vertical = Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Min(3),
+            Constraint::Length(3),
+        ]);
+        let [top_bar, middle_area, bottom_bar] = vertical.areas(area);
+        self.render_top_bar(top_bar, buf);
+    }
+
