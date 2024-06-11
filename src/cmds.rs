83bcc9ff423bf9c6c1c472c74efdc73fa59e85b6
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-11 13:45:19 -0700

    update read_words_file to check if user has load anki notes on

--- src/cmds.rs
+++ src/cmds.rs
@@ -1,3 +1,4 @@
+use crate::anki::{return_new_anki_words, ConfigJson};
 use crate::app::*;
 use arboard::Clipboard;
 use std::fs::{File, OpenOptions};
@@ -111,7 +112,7 @@ impl AppState {
         Ok(())
     }
 
-    pub fn read_words_file(&mut self) -> io::Result<()> {
+    pub async fn read_words_file(&mut self) -> io::Result<()> {
         let file = File::open("words.txt")?;
         let reader = BufReader::new(file);
 
@@ -122,6 +123,23 @@ impl AppState {
                 self.expressions
                     .push(Expression::from(word.to_string(), None, None));
             }
+        }
+
+        if self.config.options.auto_load_new_notes {
+            match return_new_anki_words(&self.client, &self.config).await {
+                Ok(words) => {
+                    for word in words {
+                        let exp = Expression::from(word, None, None);
+                        if self.expressions.contains(&exp) {
+                            continue;
+                        }
+                        self.expressions.push(exp);
+                    }
+                }
+                Err(_) => {
+                    todo!()
+                }
+            };
         }
 
         Ok(())
@@ -232,7 +250,7 @@ pub fn write_to_errs_log(err_vec: &Vec<String>) -> std::io::Result<()> {
         .append(true)
         .create(true)
         .open("err_log.txt")?;
-    
+
     let mut writer = BufWriter::new(file);
 
     for err in err_vec {
